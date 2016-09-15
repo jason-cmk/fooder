7045ec15d4a8b1068923408dabdc3168c8d50d72
jason-cmk <jas.kang@gmail.com>
2016-09-15 15:39:18 +1200

    removed unwanted modules

--- js/index.js
+++ js/index.js
@@ -33,6 +33,7 @@ function search() {
       beforeSend: function (xhrObj) {
             // Request headers
             xhrObj.setRequestHeader("user-key", "281e20b69128a60717aa2fb9202470c2");
+            xhrObj.setRequestHeader("Access-Control-Allow-Origin", "http://fooder-search.azurewebsites.net/")
         },
         type: "POST",
     })
